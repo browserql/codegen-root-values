4d4497b2e1329c70420c593fac20633cadb42487
pierrot <pierre@drouillet.es>
2021-11-14 17:55:24 +0100

    first

---
+++ handler.ts
@@ -0,0 +1,16 @@
+import {
+  DocumentNode,
+  GraphQLSchema
+} from 'graphql';
+import { Parser, ParserArguments } from './classes/Parser';
+
+interface Schema {
+  source: string;
+  document: DocumentNode;
+  schema: GraphQLSchema;
+  arguments?: ParserArguments;
+}
+
+export async function handler({ document, arguments: args }: Schema) {
+  return Parser.parse(document, args);
+}
